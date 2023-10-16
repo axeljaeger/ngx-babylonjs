39d6a4c8d8d64d7d0951ab6adc96a8846940a702
Axel Jäger <axeljaeger@gmail.com>
2023-10-16 18:58:51 +0200

    Add example applications

--- apps/playground-example/jest.config.ts
+++ apps/playground-example/jest.config.ts
@@ -1,9 +1,9 @@
 /* eslint-disable */
 export default {
   displayName: 'playground-example',
-  preset: '../jest.preset.js',
+  preset: '../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
-  coverageDirectory: '../coverage/playground-example',
+  coverageDirectory: '../../coverage/apps/playground-example',
   transform: {
     '^.+\\.(ts|mjs|js|html)$': [
       'jest-preset-angular',
